57a41e8b331cd0216f2fea336d5297396641c7af
PJ Frias <peejfrias@gmail.com>
2018-01-23 18:26:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,6 @@ class Student
       VALUES (?,?)
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
     @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")[0][0]
   end
 
