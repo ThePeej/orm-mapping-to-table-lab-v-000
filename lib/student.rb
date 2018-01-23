a7548534732eb0375dc652b2b6f10176506925c6
PJ Frias <peejfrias@gmail.com>
2018-01-23 17:57:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,12 @@
 class Student
-
+  attr_accessor :name, :grade, :id
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
+  end
+
   def self.create_table
 
   end
